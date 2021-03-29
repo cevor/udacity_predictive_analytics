fc7b95962b40aa8c11fa42b688bc4d2a9dfdcef6
cevor <info.cevor@gmail.com>
2021-03-29 20:12:24 +0000

    start jupyterlab

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8888:8888 --rm --name udacity_notebook -v "$PWD":/home/jovyan/work jupyter/scipy-notebook
+docker run -d -p 8888:8888 --rm --name udacity_notebook -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/scipy-notebook
